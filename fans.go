91e7b054384f34a28c6acf55777fd01429e792de
Andrew Arrow <oneone@gmail.com>
2021-06-25 19:51:08 -0700

    adding sql queries

--- fans.go
+++ fans.go
@@ -2,15 +2,21 @@ package main
 
 import (
 	"net/http"
+	"sync"
 
+	"github.com/andrewarrow/cloutcli"
 	"github.com/gin-gonic/gin"
 )
 
+var usernameMutex sync.Mutex
+
 func BiggestFanOfShow(c *gin.Context) {
 	username := c.Param("username")
-	likes := []string{"wfwe"}
-	reclouts := []string{"wfwefew", "Wfwe"}
-	diamonds := []string{"wfwe"}
+	//usernameMutex.Lock()
+	likes := cloutcli.QuerySqliteTopLikers(username, "50")
+	reclouts := cloutcli.QuerySqliteTopReclouters(username, "50")
+	diamonds := cloutcli.QuerySqliteTopDiamondGivers(username, "50")
+	//usernameMutex.Unlock()
 	c.HTML(http.StatusOK, "biggest_fan_of.tmpl",
 		gin.H{"username": username,
 			"likes": likes, "reclouts": reclouts, "diamonds": diamonds})
