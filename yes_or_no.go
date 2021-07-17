2ef80d64ed904cc56c0793fbae37ecd0d1a75ab7
Andrew Arrow <oneone@gmail.com>
2021-07-17 16:35:33 -0700

    adding yes or no buttons

--- yes_or_no.go
+++ yes_or_no.go
@@ -10,6 +10,7 @@ import (
 func YesOrNoIndex(c *gin.Context) {
 	pub58, _ := c.Cookie("httpclout_pub58")
 	ranPost := Last100Posts[rand.Intn(100)]
+	//ranPost := lib.MiniPost{}
 	if pub58 == "" {
 		c.HTML(http.StatusOK, "yes_or_no.tmpl", gin.H{"Post": ranPost})
 	}
