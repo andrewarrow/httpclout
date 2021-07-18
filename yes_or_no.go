4ec425e5fb8ed81d775e9590d9d25dee32cada69
Andrew Arrow <oneone@gmail.com>
2021-07-18 05:03:49 -0700

    adding the logout logic and setting up postHashHex for login or diamond

--- yes_or_no.go
+++ yes_or_no.go
@@ -1,17 +1,15 @@
 package main
 
 import (
-	"math/rand"
 	"net/http"
 
+	"github.com/andrewarrow/mini/lib"
 	"github.com/gin-gonic/gin"
 )
 
 func YesOrNoIndex(c *gin.Context) {
-	pub58, _ := c.Cookie("httpclout_pub58")
-	ranPost := Last100Posts[rand.Intn(100)]
-	//ranPost := lib.MiniPost{}
-	if pub58 == "" {
-		c.HTML(http.StatusOK, "yes_or_no.tmpl", gin.H{"Post": ranPost})
-	}
+	pub58, _ := c.Cookie("identity_pub58")
+	//ranPost := Last100Posts[rand.Intn(100)]
+	ranPost := lib.MiniPost{}
+	c.HTML(http.StatusOK, "yes_or_no.tmpl", gin.H{"pub58": pub58, "Post": ranPost})
 }
