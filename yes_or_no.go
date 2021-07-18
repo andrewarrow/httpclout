fc1cedd2a56da821ad4c01634ff51a1161ad60b0
Andrew Arrow <oneone@gmail.com>
2021-07-18 06:39:09 -0700

    adding back the real random messages

--- yes_or_no.go
+++ yes_or_no.go
@@ -1,18 +1,18 @@
 package main
 
 import (
+	"math/rand"
 	"net/http"
 
-	"github.com/andrewarrow/mini/lib"
 	"github.com/gin-gonic/gin"
 )
 
 func YesOrNoIndex(c *gin.Context) {
 	pub58, _ := c.Cookie("identity_pub58")
-	//ranPost := Last100Posts[rand.Intn(100)]
-	ranPost := lib.MiniPost{}
-	ranPost.PostHashHex = "9ef3afa082054898b21f056211e2c6f3f145bdea62d9805eb9e59e1538d595dc"
-	ranPost.PosterPub58 = "BC1YLiCo6prb6M3xELpRbHUAtQvNAegcr2GHg1Z9LYDL52cZrbctHmr"
+	ranPost := Last100Posts[rand.Intn(100)]
+	//ranPost := lib.MiniPost{}
+	//ranPost.PostHashHex = "9ef3afa082054898b21f056211e2c6f3f145bdea62d9805eb9e59e1538d595dc"
+	//ranPost.PosterPub58 = "BC1YLiCo6prb6M3xELpRbHUAtQvNAegcr2GHg1Z9LYDL52cZrbctHmr"
 
 	successPostHashHex := c.Query("postHashHex")
 	successTheirPub58 := c.Query("theirPub58")
